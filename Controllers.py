24d019ddc6bfc9b147f26c190dc01bb4efd66d78
Pontomira <vwgadocs@gmail.com>
2015-10-21 10:01:08 +0100

    Update Controllers.py

--- Controllers.py
+++ Controllers.py
@@ -40,8 +40,8 @@ class PIDControllers():
 			
 		wheelPosition1 = -encoder1.poll_position()				# Get position from the first encoder
 		wheelPosition2 = encoder2.poll_position()					# Get position from the second encoder
-		wheelPosition1_m = (float (wheelPosition1))/float(980*self.PI*0.2032)	# Calculate the travelled distance for first encoder
-		wheelPosition2_m = (float (wheelPosition2))/float(980*self.PI*0.2032)	# Calculated the travelled distance for second encoder	
+		wheelPosition1_m = (float(wheelPosition1))/float(980*self.PI*0.2032)	# Calculate the travelled distance for first encoder
+		wheelPosition2_m = (float(wheelPosition2))/float(980*self.PI*0.2032)	# Calculated the travelled distance for second encoder	
 	
 		wheelPosition = float(wheelPosition1_m + wheelPosition2_m)/2		# Average travelled distance from the robot
 	
@@ -49,28 +49,28 @@ class PIDControllers():
 	
 		self.LastwheelPosition = wheelPosition					# Keep the position for next cycle
 	
-		self.errorVelocity = float (PiVelocityRef) - float (wheelVelocity)		# Error for requested velocity
+		self.errorVelocity = float(PiVelocityRef) - float(wheelVelocity)		# Error for requested velocity
 		pTermVelocity = self.KpVelocity * float(self.errorVelocity)			# Proportional term of the error
-		self.integrated_errorVelocity += float (self.errorVelocity)		# Integral error
-		iTermVelocity = self.KiVelocity * float (self.integrated_errorVelocity)	# Integral term of the error
-		PiAngleRef = float (pTermVelocity) + float (iTermVelocity)			# Velocity PID result
+		self.integrated_errorVelocity += float(self.errorVelocity)		# Integral error
+		iTermVelocity = self.KiVelocity * float(self.integrated_errorVelocity)	# Integral term of the error
+		PiAngleRef = float(pTermVelocity) + float(iTermVelocity)			# Velocity PID result
 	
 		return -PiAngleRef
 	
 	def PidAngle(self, x_angle, PiAngleRef):
 
-		self.errorAngle = float (PiAngleRef) - float (x_angle)			# Error for requested angle
-		pTermAngle = self.KpAngle * float (self.errorAngle)			# Proportional term of the error
-		self.integrated_errorAngle += float (self.errorAngle)			# Integral error
+		self.errorAngle = float(PiAngleRef) - float(x_angle)			# Error for requested angle
+		pTermAngle = self.KpAngle * float(self.errorAngle)			# Proportional term of the error
+		self.integrated_errorAngle += float(self.errorAngle)			# Integral error
 		if self.integrated_errorAngle < -35:					# Integral limits
 			self.integrated_errorAngle = -35
 		if self.integrated_errorAngle > 35:
 			self.integrated_errorAngle = 35
 	
-		iTermAngle = self.KiAngle * float (self.integrated_errorAngle)			# Integral term of the error
+		iTermAngle = self.KiAngle * float(self.integrated_errorAngle)			# Integral term of the error
 		dTermAngle = self.KdAngle * (self.errorAngle - self.last_errorAngle)		# Derivative term of the error
 		self.last_errorAngle = self.errorAngle						# Keep actual value
-		PidMotorRef = float (pTermAngle) + float (iTermAngle) + float (dTermAngle)	# Angle PID result
+		PidMotorRef = float(pTermAngle) + float(iTermAngle) + float(dTermAngle)	# Angle PID result
 	
 		if PidMotorRef < -127:								# Limits for motor reference
 			PidMotorRef = -127
