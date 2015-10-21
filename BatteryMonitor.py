a8327de5e35b5fa38249719017e8750f6f06d9eb
Pontomira <vwgadocs@gmail.com>
2015-10-21 10:23:06 +0100

    Update BatteryMonitor.py

--- BatteryMonitor.py
+++ BatteryMonitor.py
@@ -8,10 +8,10 @@ import Adafruit_BBIO.GPIO as GPIO
 ADC.setup()
 
 # Pin assigment
-GreenBattery="P9_15"
-YellowBattery="P9_12"
-RedBattery="P9_23"
-AnalogPin="P9_38"
+GreenBattery = "P9_15"
+YellowBattery = "P9_12"
+RedBattery = "P9_23"
+AnalogPin = "P9_38"
 
 # Pin setup
 GPIO.setup(GreenBattery, GPIO.OUT)
@@ -23,21 +23,20 @@ GPIO.setup(RedBattery, GPIO.OUT)
 # The battery used is a 12V nominal voltage, so the margin for voltage measurement is up to 13V
 class BatteryValue():
 	def BatteryVoltage(self):
-		BatteryVoltageVal=ADC.read(AnalogPin)
-		BatteryVoltageVolt=float (BatteryVoltageVal*1.8)		# ADC voltage measured
-		BatteryRealValue=float (7.2292*BatteryVoltageVolt)		# Real Voltage
+		BatteryVoltageVal = ADC.read(AnalogPin)
+		BatteryRealValue = float(13.01256*BatteryVoltageVal)		# Real Voltage
 
-		if BatteryRealValue<10.5:					# Define Red region, under 10,5V
+		if BatteryRealValue < 10.5:					# Define Red region, under 10,5V
 			GPIO.output(GreenBattery, GPIO.LOW)
 			GPIO.output(YellowBattery, GPIO.LOW)
 			GPIO.output(RedBattery, GPIO.HIGH)
 	
-		if BatteryRealValue>10.5 and BatteryRealValue<12:		# Define Yellow region, between 10,5 and 12V
+		if BatteryRealValue > 10.5 and BatteryRealValue < 12:		# Define Yellow region, between 10,5 and 12V
 			GPIO.output(GreenBattery, GPIO.LOW)
 			GPIO.output(YellowBattery, GPIO.HIGH)
 			GPIO.output(RedBattery, GPIO.LOW)	
 	
-		if BatteryRealValue>12:						# Define the green region, over 12V
+		if BatteryRealValue > 12:					# Define the green region, over 12V
 			GPIO.output(GreenBattery, GPIO.HIGH)
 			GPIO.output(YellowBattery, GPIO.LOW)
 			GPIO.output(RedBattery, GPIO.LOW)
