c17667bb69fc78cd9019543bb19e20247fc4cc81
VitorHugoAguiar <hugoaguiar_18@hotmail.com>
2015-12-02 14:40:20 +0000

    just a update

--- Sabertooth.py
+++ Sabertooth.py
@@ -16,12 +16,12 @@ class PacketizedCommunication():
 	baudcodes = {2400:1, 9600:2, 19200:3, 38400:4}
 	
 	# Baudrate and address for sabertooth controller
-	baud = 19200
+	baud = 38400
 	addr = 128
 
 	# Define serial connection
 	ser = serial.Serial('/dev/ttyO1',baud, timeout=0.5)
-
+	
 	# Initialization of the sabertooth connection
 	print "\nSending commands to the address",addr, "with a baudrate of\n", baud
 	ser.flush()
@@ -30,19 +30,19 @@ class PacketizedCommunication():
 
 	def set_baud(self,address, baudrate):
 		self.packet = self.make_packet(address, self.commands['baud'], self.baudcodes[baudrate])	# Packet format
-		self.ser.write(self.packet)									# Write packet in the serial
+		self.ser.write(self.packet)												# Write packet in the serial
 
-	def drive(self, address, motor, speed):									# Drive function for both motors with the commands defined above
+	def drive(self, address, motor, speed):										# Drive function for both motors with the commands defined above
 		if motor == 1:
 			command = 0
 		elif motor == 2:
 			command = 4
 		else:
 			return false
-		if speed < 0:											# Values must be always positive and direction is given by the command +1
+		if speed < 0:															# Values must be always positive and direction is given by the command +1
 			command = command + 1
 			speed = -speed
-		if speed > 127:											# Since all values are positive we only compare with 127
+		if speed > 127:															# Since all values are positive we only compare with 127
 			speed = 127
 		self.packet = self.make_packet(address, command, speed)
 		self.ser.write(self.packet)
@@ -50,8 +50,9 @@ class PacketizedCommunication():
 	def make_packet(self, address, command, data):								# Packet struct
 		return struct.pack('BBBB', address, command, data, (127&(address+command+data)))
 
-	def stopAndReset(self):											# Stop and Reset function to begin or end
+	def stopAndReset(self):														# Stop and Reset function to begin or end
 		self.drive(self.addr, 1, int(0))
 		self.drive(self.addr, 2, int(0))
+		self.ser.flush()
 		
 		
