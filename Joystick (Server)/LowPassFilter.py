b494eba61a706ea8cac090cf8aac2cc550b4ef03
VitorHugoAguiar <hugoaguiar_18@hotmail.com>
2016-01-11 18:24:36 +0000

    Just updating files

---
+++ Joystick (Server)/LowPassFilter.py
@@ -0,0 +1,20 @@
+filteredDataFR=[0,0,0,0]
+filteredDataLR=[0,0,0,0]
+LPFgainFR=0.1
+LPFgainLR=0.33
+
+class LowPassFilter():
+	def lowPassFilterFR(self, directionForwardReverse):	
+		
+		filteredDataFR[0]=directionForwardReverse*LPFgainFR+filteredDataFR[1]*(1-LPFgainFR)
+		filteredDataFR[1]=filteredDataFR[0]
+
+		return filteredDataFR[0]
+		
+	
+	def lowPassFilterLR(self, directionLeftTRight):
+		
+		filteredDataLR[0]=directionLeftTRight*LPFgainLR+filteredDataLR[1]*(1-LPFgainLR)
+		filteredDataLR[1]=filteredDataLR[0]
+
+		return filteredDataLR[0]
---
+++ Joystick (Server)/graph.py~
@@ -0,0 +1,3 @@
+import matplotlib.pylab as plt
+
+
