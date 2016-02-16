e9b269824a4d7fc12b0e50ef765fb80f82143059
VitorHugoAguiar <hugoaguiar_18@hotmail.com>
2016-02-16 15:06:17 +0000

    remove ProBot

--- ProBot (BeagleBone)/EnableEncoders.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/sh
-# first program to run to enable the readings from the encoders
-sudo echo bone_eqep0 > /sys/devices/bone_capemgr.9/slots
-sudo echo bone_eqep2b > /sys/devices/bone_capemgr.9/slots
