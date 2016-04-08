5b24296b409e87ee050b97353c8eb9216030812c
Vitor Hugo Abreu de Aguiar <hugoaguiar_18@hotmail.com>
2016-04-08 11:21:05 +0100

    Update RestartProgram.py

--- ProBot_BeagleBone/RestartProgram.py
+++ ProBot_BeagleBone/RestartProgram.py
@@ -9,6 +9,7 @@ Pub_Sub = SocketCommunication.publisher_and_subscriber()
 class Restart():
     def RestartProgram (self):
 	while True:
+		# Checking if the program needs to restart
 		restartVar = Pub_Sub.subscriber2()
 		if restartVar is None:
     			restartVar = 0
@@ -16,11 +17,11 @@ class Restart():
 			userChoice1 = '0'
 		else:
     			userChoice, userChoice1 = restartVar.split()
+    			# Sending the previous value of the user's choices to the ProBot.py file
 			if userChoice=='0':
 				python = sys.executable
     				os.execl(python, python, * sys.argv)
 			publisher2=Pub_Sub.publisher2(userChoice, userChoice1)
-			#print userChoice, userChoice1
 
 if __name__ == '__main__':
 	Restart=Restart()
