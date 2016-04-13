0247ead9c68ec4a7cb0f6bde63f6b3629f4cd4d2
Vitor Hugo Abreu de Aguiar <hugoaguiar_18@hotmail.com>
2016-04-13 21:48:37 +0100

    Update install_bootScripts.sh

--- ProBot_BeagleBone/install_bootScripts.sh
+++ ProBot_BeagleBone/install_bootScripts.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -ex
+(sudo crontab -l ; echo "@reboot sleep 20 && /home/machinekit/ProBot/ProBot_BeagleBone/WebClient.py ws://139.162.157.96:9000") |sudo crontab -
 (sudo crontab -l ; echo "@reboot sh /home/machinekit/ProBot/ProBot_BeagleBone/EnableEncoders.sh") |sudo crontab -
-(sudo crontab -l ; echo "@reboot sh /home/machinekit/ProBot/ProBot_BeagleBone/forward_ZMQ_Client") |sudo crontab -
-(sudo crontab -l ; echo "@reboot sh /home/machinekit/ProBot/ProBot_BeagleBone/WebClient.py ws://139.162.157.96:9000") |sudo crontab -
-(sudo crontab -l ; echo "@reboot sh /home/machinekit/ProBot/ProBot_BeagleBone/RestartProgram_ZMQ.py") |sudo crontab -
-(sudo crontab -l ; echo "@reboot sh /home/machinekit/ProBot/ProBot_BeagleBone/RestartProgram.py") |sudo crontab -
+(sudo crontab -l ; echo "@reboot python /home/machinekit/ProBot/ProBot_BeagleBone/forward_ZMQ_Client.py") |sudo crontab -
+(sudo crontab -l ; echo "@reboot python /home/machinekit/ProBot/ProBot_BeagleBone/RestartProgram_ZMQ.py") |sudo crontab -
+(sudo crontab -l ; echo "@reboot python /home/machinekit/ProBot/ProBot_BeagleBone/RestartProgram.py") |sudo crontab -
