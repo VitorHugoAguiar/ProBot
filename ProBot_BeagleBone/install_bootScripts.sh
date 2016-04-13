6758b1f310678092f29849c1fbd097045b8f5735
Vitor Hugo Abreu de Aguiar <hugoaguiar_18@hotmail.com>
2016-04-13 21:09:01 +0100

    Update install_bootScripts.sh

--- ProBot_BeagleBone/install_bootScripts.sh
+++ ProBot_BeagleBone/install_bootScripts.sh
@@ -3,3 +3,5 @@ set -ex
 (sudo crontab -l ; echo "@reboot sh /home/machinekit/ProBot/ProBot_BeagleBone/EnableEncoders.sh") |sudo crontab -
 (sudo crontab -l ; echo "@reboot sh /home/machinekit/ProBot/ProBot_BeagleBone/forward_ZMQ_Client") |sudo crontab -
 (sudo crontab -l ; echo "@reboot sh /home/machinekit/ProBot/ProBot_BeagleBone/WebClient.py ws://139.162.157.96:9000") |sudo crontab -
+(sudo crontab -l ; echo "@reboot sh /home/machinekit/ProBot/ProBot_BeagleBone/RestartProgram_ZMQ.py") |sudo crontab -
+(sudo crontab -l ; echo "@reboot sh /home/machinekit/ProBot/ProBot_BeagleBone/RestartProgram.py") |sudo crontab -
