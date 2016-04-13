6c7eb175009787726bbf48759bb45ab74ece980c
Vitor Hugo Abreu de Aguiar <hugoaguiar_18@hotmail.com>
2016-04-13 20:07:35 +0100

    Update install_Encoders.sh

--- ProBot_BeagleBone/install_Encoders.sh
+++ ProBot_BeagleBone/install_Encoders.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -ex
-cd ProBot/ProBot_BeagleBone/encoders
+cd encoders
 sudo cp bone_eqep0-00A0.dtbo /lib/firmware
 sudo cp bone_eqep1-00A0.dtbo /lib/firmware
 sudo cp bone_eqep2-00A0.dtbo /lib/firmware
