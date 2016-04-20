71a5bb798d24ce33b27718f69ef066f2be2042fa
Vitor Hugo Abreu de Aguiar <hugoaguiar_18@hotmail.com>
2016-04-20 17:11:39 +0100

    Update install_everythingProBot.sh

--- ProBot_BeagleBone/install_everythingProBot.sh
+++ ProBot_BeagleBone/install_everythingProBot.sh
@@ -49,15 +49,16 @@ sudo rm -rf /etc/apt/apt.conf.d/02compress-indexes
 
 sudo apt-get update
 
+sudo apt-get install -y -t sid libczmq-dev
+sudo apt-get install -y apt-show-versions
+
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 43DDF224
 sudo sh -c \
   "echo 'deb http://deb.machinekit.io/debian jessie main' > \
   /etc/apt/sources.list.d/machinekit.list"
-
+  
 sudo apt-get update
 
-sudo apt-get install -y -t sid libczmq-dev
-sudo apt-get install -y apt-show-versions
 sudo apt-get install -y linux-image-3.8.13-xenomai-r78 
 sudo apt-get install -y machinekit-xenomai machinekit-dev
 
