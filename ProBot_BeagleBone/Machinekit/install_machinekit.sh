ade63f44e3447d153220f98ce893a331e6128d03
Vitor Hugo Abreu de Aguiar <hugoaguiar_18@hotmail.com>
2016-06-02 17:26:56 +0100

    Update install_machinekit.sh

--- ProBot_BeagleBone/Machinekit/install_machinekit.sh
+++ ProBot_BeagleBone/Machinekit/install_machinekit.sh
@@ -12,12 +12,6 @@ Pin: release o=Debian
 Pin-Priority: -10' > \
 /etc/apt/preferences.d/sid;"
 
-(sudo crontab -l ; echo "@reboot sleep 20 && python $(pwd -P)/WebClient.py $input_variable:$input_variable2") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
-(sudo crontab -l ; echo "@reboot sh $(pwd -P)/EnableEncoders.sh") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
-(sudo crontab -l ; echo "@reboot python $(pwd -P)/forward_ZMQ_Client.py") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
-(sudo crontab -l ; echo "@reboot python $(pwd -P)/RestartProgram_ZMQ.py") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
-(sudo crontab -l ; echo "@reboot python $(pwd -P)/RestartProgram.py") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
-
 sudo rm -rf /etc/apt/apt.conf.d/02compress-indexes 
 
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 43DDF224
