6dafc2329a3560d16107e31b0ad0ac8461900681
Vitor Hugo Abreu de Aguiar <hugoaguiar_18@hotmail.com>
2016-06-08 10:17:21 +0100

    Update install_everythingProBot.sh

--- ProBot_BeagleBone/install_everythingProBot.sh
+++ ProBot_BeagleBone/install_everythingProBot.sh
@@ -30,8 +30,6 @@ export LC_TYPE=en_US.UTF-8
 (sudo crontab -l ; echo "@reboot sleep 20 && python $(pwd -P)/WebClient.py ws://$input_variable:$input_variable2") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
 (sudo crontab -l ; echo "@reboot sh $(pwd -P)/EnableEncoders.sh") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
 (sudo crontab -l ; echo "@reboot python $(pwd -P)/forward_ZMQ_Client.py") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
-(sudo crontab -l ; echo "@reboot python $(pwd -P)/RestartProgram_ZMQ.py") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
-(sudo crontab -l ; echo "@reboot python $(pwd -P)/RestartProgram.py") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
 
 sudo sh -c \
 "echo 'Package: *
