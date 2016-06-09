2a76631a5ddb6e215cfdaae61b4a6da8a230d54f
Vitor Hugo Abreu de Aguiar <hugoaguiar_18@hotmail.com>
2016-06-09 11:08:04 +0100

    Update Encoders.py

--- ProBot_BeagleBone/Encoders.py
+++ ProBot_BeagleBone/Encoders.py
@@ -13,7 +13,6 @@ encoder1 = eqep.eQEP("/sys/devices/ocp.3/48302000.epwmss/48302180.eqep", eqep.eQ
 # Instantiate an instance of the driver for encoder of the motor 2
 encoder2 = eqep.eQEP("/sys/devices/ocp.3/48304000.epwmss/48304180.eqep", eqep.eQEP.MODE_ABSOLUTE)
 
-
 class EncodersReadings():
     def __init__(self, LastwheelPosition1=0, LastwheelPosition2=0):
         self.LastwheelPosition1 = LastwheelPosition1
@@ -24,8 +23,8 @@ class EncodersReadings():
         wheelPosition1 = encoder1.get_position()					# Get position from the first encoder
         wheelPosition2 = encoder2.get_position()					# Get position from the second encoder
 
-        wheelPosition1_m = (float(wheelPosition1)) / Pconst.ticks * math.pi * Pconst.wheelDiameter	# Calculate the travelled distance for first encoder
-        wheelPosition2_m = (float(wheelPosition2)) / Pconst.ticks * math.pi * Pconst.wheelDiameter	# Calculated the travelled distance for second encoder
+        wheelPosition1_m = (float(wheelPosition1)) / Pconst.ticks * math.pi * Pconst.wheelDiameter	# First wheel distance travelled 
+        wheelPosition2_m = (float(wheelPosition2)) / Pconst.ticks * math.pi * Pconst.wheelDiameter	# Second wheel distance travelled
 
         wheelVelocity1 = float(wheelPosition1_m - self.LastwheelPosition1)				# Wheel velocity
 	wheelVelocity2 = float(wheelPosition2_m - self.LastwheelPosition2)   		
