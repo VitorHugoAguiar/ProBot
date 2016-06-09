f30b3cc38725024c3b3acec84ce44b30dcd40d01
Vitor Hugo Abreu de Aguiar <hugoaguiar_18@hotmail.com>
2016-06-09 12:35:00 +0100

    Update install_everythingProBot.sh

--- ProBot_BeagleBone/install_everythingProBot.sh
+++ ProBot_BeagleBone/install_everythingProBot.sh
@@ -52,7 +52,6 @@ sudo apt-get install -y -t sid libczmq-dev
 
 sudo rm -rf /etc/apt/apt.conf.d/02compress-indexes 
 sudo apt-get update
-sudo apt-get install -y xauth
 sudo apt-get install -y apt-show-versions
 
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 43DDF224
