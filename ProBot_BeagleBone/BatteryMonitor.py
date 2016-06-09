ec4526dd775e519fb34aca601b3ee59acea0840e
Vitor Hugo Abreu de Aguiar <hugoaguiar_18@hotmail.com>
2016-06-09 11:01:00 +0100

    Update BatteryMonitor.py

--- ProBot_BeagleBone/BatteryMonitor.py
+++ ProBot_BeagleBone/BatteryMonitor.py
@@ -16,11 +16,11 @@ ADC.setup()
 class BatteryVoltage():
 
         def VoltageValue(self, type):
-            # Reading from the LiPo battery
+            # Reading the voltage from the LiPo battery
             options = {'LiPo': [Pconst.AnalogPinLiPo, Pconst.mLiPo, Pconst.MinRedLiPo, Pconst.GreenLED, Pconst.RedLED]}
             voltageVar = options[type]
 
-            BatteryVoltageVal = ADC.read(voltageVar[0])                                         # Readings from the ADC battery
+            BatteryVoltageVal = ADC.read(voltageVar[0])                                         # ADC readings from the battery
             BatteryRealValue = float((voltageVar[1]*BatteryVoltageVal))          		# Real voltage from the LiPo battery
 	    
             if BatteryRealValue < voltageVar[2]:					        # Definition of the Red region for the battery
