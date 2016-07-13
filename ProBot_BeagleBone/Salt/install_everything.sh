0b2a8c96b24fc1f2ecb9463b32045d888dfd9965
Vitor Hugo Abreu de Aguiar <hugoaguiar_18@hotmail.com>
2016-07-13 10:18:08 +0100

    Update install_everything.sh

--- ProBot_BeagleBone/Salt/install_everything.sh
+++ ProBot_BeagleBone/Salt/install_everything.sh
@@ -5,11 +5,11 @@ export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LC_TYPE=en_US.UTF-8
 
+cd ..
 (sudo crontab -l ; echo "@reboot sleep 20 && python $(pwd -P)/WebClient.py ws://192.168.10.236:9000") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
 (sudo crontab -l ; echo "@reboot sh $(pwd -P)/EnableEncoders.sh") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
 (sudo crontab -l ; echo "@reboot python $(pwd -P)/forward_ZMQ_Client.py") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
 
-cd ..
 cd Machinekit
 sudo sh install_Machinekit.sh
 
