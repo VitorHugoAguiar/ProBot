b045e0794e46dd7d90be10b798e586d998801cf1
Vitor Hugo Abreu de Aguiar <hugoaguiar_18@hotmail.com>
2016-07-19 17:38:25 +0100

    Update install_everythingProBot.sh

--- ProBot_BeagleBone/InstallEverything/install_everythingProBot.sh
+++ ProBot_BeagleBone/InstallEverything/install_everythingProBot.sh
@@ -27,6 +27,8 @@ export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LC_TYPE=en_US.UTF-8
 
+sudo apt-get install -y python-smbus
+
 cd..
 (sudo crontab -l ; echo "@reboot sleep 20 && python $(pwd -P)/WebClient.py ws://$input_variable:$input_variable2") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
 (sudo crontab -l ; echo "@reboot sh $(pwd -P)/EnableEncoders.sh") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
