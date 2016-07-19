a9e5cb79ba67850a8a2059b461b7081c68e25df5
Vitor Hugo Abreu de Aguiar <hugoaguiar_18@hotmail.com>
2016-07-19 18:09:12 +0100

    Update install_SaltMinion.sh

--- ProBot_BeagleBone/SaltMinion/install_SaltMinion.sh
+++ ProBot_BeagleBone/SaltMinion/install_SaltMinion.sh
@@ -3,7 +3,7 @@
 sudo grep -q -F 'deb http://httpredir.debian.org/debian jessie-backports main' /etc/apt/sources.list || echo 'deb http://httpredir.debian.org/debian jessie-backports main' >> /etc/apt/sources.list
 sudo grep -q -F 'deb http://httpredir.debian.org/debian stretch main'  /etc/apt/sources.list || echo 'deb http://httpredir.debian.org/debian stretch main' >> /etc/apt/sources.list
 
-sudo grep -q -F 'APT::Default-Release "jessie";' /etc/apt/apt.conf.d/10apt || echo 'APT::Default-Release "jessie";' >> /etc/apt/apt.conf.d/10apt
+sudo echo 'APT::Default-Release "jessie";' >> /etc/apt/apt.conf.d/10apt || grep -q -F 'APT::Default-Release "jessie";' /etc/apt/apt.conf.d/10apt
 
 sudo apt-get update
 sudo apt-get install -y python-zmq python-tornado/jessie-backports salt-common/stretch
