608bd3a217e2f320989457275763f209bec30e02
Vitor Hugo Abreu de Aguiar <hugoaguiar_18@hotmail.com>
2016-07-19 17:49:24 +0100

    Update install_everythingProBot.sh

--- ProBot_BeagleBone/InstallEverything/install_everythingProBot.sh
+++ ProBot_BeagleBone/InstallEverything/install_everythingProBot.sh
@@ -27,14 +27,13 @@ export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LC_TYPE=en_US.UTF-8
 
-sudo apt-get install -y python-smbus
-sudo apt-get -y install network-manager
-sudo apt-get -y install firmware-ralink 
-
 cd..
 (sudo crontab -l ; echo "@reboot sleep 20 && python $(pwd -P)/WebClient.py ws://$input_variable:$input_variable2") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
 (sudo crontab -l ; echo "@reboot sh $(pwd -P)/EnableEncoders.sh") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
 (sudo crontab -l ; echo "@reboot python $(pwd -P)/forward_ZMQ_Client.py") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
+
+sudo apt-get -y install network-manager
+sudo apt-get -y install firmware-ralink 
 
 cd Machinekit
 sudo sh install_Machinekit.sh
