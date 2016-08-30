a3a30eeaeb551a8e7c6f41a765836ce71f36c6c1
Vitor Hugo Abreu de Aguiar <hugoaguiar_18@hotmail.com>
2016-08-30 19:42:52 +0100

    Update install_everythingProBot.sh

--- ProBot_BeagleBone/InstallEverything/install_everythingProBot.sh
+++ ProBot_BeagleBone/InstallEverything/install_everythingProBot.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-echo "Please enter the ProBot Server ip: "
+echo "Please enter the ProBot's Server ip: "
 read input_variable
-echo "Please enter the port that you gonna use: "
-read input_variable2
+
 echo "Your ip is: $input_variable"
-echo "Your port is: $input_variable2"
+
 
 echo -n "Confirm (Y/N): "
 read answer
@@ -23,7 +22,7 @@ else
 fi
 
 cd ..
-(sudo crontab -l ; echo "@reboot sleep 20 && python $(pwd -P)/WebClient.py ws://$input_variable:$input_variable2") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
+(sudo crontab -l ; echo "@reboot sleep 20 && python $(pwd -P)/WebClient.py") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
 (sudo crontab -l ; echo "@reboot sh $(pwd -P)/EnableEncoders.sh") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
 (sudo crontab -l ; echo "@reboot python $(pwd -P)/forward_ZMQ_Client.py") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
 
