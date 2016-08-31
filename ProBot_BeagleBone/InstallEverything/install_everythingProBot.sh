fb384cd87cb9d090f0b8ff3135473083da2479af
VitorHugoAguiar <hugoaguiar_18@hotmail.com>
2016-08-31 09:37:35 +0000

    change the installion script

--- ProBot_BeagleBone/InstallEverything/install_everythingProBot.sh
+++ ProBot_BeagleBone/InstallEverything/install_everythingProBot.sh
@@ -1,34 +1,8 @@
 #!/bin/sh
 
-<<<<<<< HEAD
 cd ..
 
 sudo python WebServerIp.py
-=======
-echo "Please enter the ProBot's Server ip: "
-read input_variable
-
-echo "Your ip is: $input_variable"
-
-
-echo -n "Confirm (Y/N): "
-read answer
-
-if echo "$answer" | grep -iq "^n" ;then
-    echo "Try again please!!!"
-    bash install_everythingProBot.sh
-fi
-
-if echo "$answer" | grep -iq "^y" ;then
-    echo "OK!"
-else
-    echo "Learn to write please X)"
-    bash install_everythingProBot.sh
-fi
-
-cd ..
-
->>>>>>> c424329244187a409e9e89f6bb5e82f8d04c7c56
 
 (sudo crontab -l ; echo "@reboot sleep 20 && python $(pwd -P)/WebClient.py") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
 (sudo crontab -l ; echo "@reboot sh $(pwd -P)/EnableEncoders.sh") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
