33d031771249a0c331f66b2ac60b30ff52c61b42
VitorHugoAguiar <hugoaguiar_18@hotmail.com>
2016-11-30 18:38:35 +0000

    fixed the websocket

--- ProBot_BeagleBone/WebClient.py
+++ ProBot_BeagleBone/WebClient.py
@@ -3,12 +3,16 @@
 
 from __future__ import print_function
 from os import environ
+import six
 
 from twisted.internet.defer import inlineCallbacks
 from autobahn.twisted.util import sleep
 
 from autobahn.twisted.wamp import ApplicationSession, ApplicationRunner
 from twisted.logger import Logger
+from twisted.internet._sslverify import OpenSSLCertificateAuthorities
+from twisted.internet.ssl import CertificateOptions
+from OpenSSL import crypto
 
 import sys
 import os
@@ -57,11 +61,19 @@ class AppSession(ApplicationSession):
                 yield sleep(1)
 
 if __name__ == '__main__':
-        runner = ApplicationRunner(
-            environ.get("AUTOBAHN_DEMO_ROUTER", u"ws://89.109.64.175:8080/ws"),
-                    u"realm1",       
-            )
-        runner.run(AppSession, auto_reconnect=True)
-
-
-
+    # load the self-signed cert the server is using
+    cert = crypto.load_certificate(
+        crypto.FILETYPE_PEM,
+        six.u(open('./server.crt', 'r').read())
+    )
+    # tell Twisted to use just the one certificate we loaded to verify connections
+    options = CertificateOptions(
+        trustRoot=OpenSSLCertificateAuthorities([cert]),
+    )
+    # ...which we pass as "ssl=" to ApplicationRunner (passed to SSL4ClientEndpoint)
+    runner = ApplicationRunner(
+        environ.get("AUTOBAHN_DEMO_ROUTER", u"wss://89.109.64.175:8080/ws"),
+        u"realm1",
+        ssl=options,  # try removing this, but still use self-signed cert
+    )
+runner.run(AppSession)
