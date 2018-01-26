74c97290af92022db149f49e8890ba9d7dffe153
VitorHugoAguiar <hugoaguiar_18@hotmail.com>
2018-01-26 16:48:27 +0000

    improve user control

--- ProBot_BeagleBone/ProBot.py
+++ ProBot_BeagleBone/ProBot.py
@@ -107,27 +107,27 @@ class ProBot():
 
 		    	# Reading the values from the webpage
                     	VelocityRef, TurnMotorRight, TurnMotorLeft = WebPage.WebPage_Values()
-
+						
 			# Reading the MPU6050 values and use the complementary filter to get better values
 		    	ComplementaryAngle=mpu6050.Complementary_filter(self.LoopTimeResult)
 
 			# Checking if the angle is out of range
 		    	if ComplementaryAngle<-20 or ComplementaryAngle>20:
 				RestartProgram.RestartProgramRoutine()
-
-		    	# With the values from the WebPage, we can calculate the outputs from the controllers
-               	    	VelocityController1 = PID.standardPID((VelocityRef+TurnMotorRight), self.wheelVelocity1, 'Velocity1', userChoice)
-                    	VelocityController2 = PID.standardPID((VelocityRef+TurnMotorLeft), self.wheelVelocity2, 'Velocity2', userChoice)
-
-		    	rightMotor = PID.standardPID(round (VelocityController1, 2), ComplementaryAngle, 'Angle1', userChoice)
-                    	leftMotor = PID.standardPID(round (VelocityController2, 2), ComplementaryAngle, 'Angle2', userChoice)
-
+				
+			# With the values from the WebPage, we can calculate the outputs from the controllers
+               	    	TargetAngle1 = PID.standardPID((VelocityRef+TurnMotorRight), self.wheelVelocity1, 'Velocity1', userChoice)
+                    	TargetAngle2 = PID.standardPID((VelocityRef+TurnMotorLeft), self.wheelVelocity2, 'Velocity2', userChoice)
+						
+		    	rightMotor = PID.standardPID(TargetAngle1, ComplementaryAngle, 'Angle1', userChoice)
+                    	leftMotor = PID.standardPID(TargetAngle2, ComplementaryAngle, 'Angle2', userChoice)
+			
  		    	# Sending the right values to the Sabertooth or the PWM controller
 	            	MotorsControlSignals.MotorsControl(rightMotor, leftMotor, userChoice)
 
 		    	LoopTimeEnd=time.time()
 	            	self.LoopTimeResult=LoopTimeEnd-LoopTimeStart
-
+			
 		    except IOError, err:
 			print(IOError, err)
 			self.bus.write_byte_data(0x68, 0x1A, 0x80)
