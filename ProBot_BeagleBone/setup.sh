3d33f69ba554eebe3b1ef570c7f6fdf360b709c4
VitorHugoAguiar <hugoaguiar_18@hotmail.com>
2018-01-29 10:29:58 +0000

    change setup

--- ProBot_BeagleBone/setup.sh
+++ ProBot_BeagleBone/setup.sh
@@ -2,7 +2,7 @@
 
 CheckInternet(){
 case "$(curl -s --max-time 2 -I http://google.com | sed 's/^[^ ]*  *\([0-9]\).*/\1/; 1q')" in
-  [23]) echo "";apt-get update -qq > /dev/null && apt-get upgrade -qq -y > /dev/null;
+  [23]) echo ""; apt-get update -qq > /dev/null && apt-get upgrade -qq -y > /dev/null;;
   5) echo "The web proxy won't let us through"
   exit 1;;
   *) echo "The network is down or very slow"
